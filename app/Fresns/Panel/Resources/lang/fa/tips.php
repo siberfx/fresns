ac0163b18b15203d3a450e8730142591782223f9
Ahmadreza Bashari <ahmadrezabashari@gmail.com>
2024-11-30 12:43:08 +0330

    Added fa lang

---
+++ app/Fresns/Panel/Resources/lang/fa/tips.php
@@ -0,0 +1,120 @@
+<?php
+
+/*
+ * Fresns (https://fresns.org)
+ * Copyright (C) 2021-Present Jevan Tang
+ * Released under the Apache-2.0 License.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Fresns Panel Tips Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'createSuccess' => 'ایجاد با موفقیت انجام شد',
+    'deleteSuccess' => 'حذف با موفقیت انجام شد',
+    'updateSuccess' => 'به‌روزرسانی با موفقیت انجام شد',
+    'upgradeSuccess' => 'ارتقاء با موفقیت انجام شد',
+    'installSuccess' => 'نصب با موفقیت انجام شد',
+    'uninstallSuccess' => 'حذف نصب با موفقیت انجام شد',
+
+    'createFailure' => 'ایجاد ناموفق بود',
+    'deleteFailure' => 'حذف ناموفق بود',
+    'updateFailure' => 'به‌روزرسانی ناموفق بود',
+    'upgradeFailure' => 'ارتقاء ناموفق بود',
+    'installFailure' => 'نصب ناموفق بود',
+    'downloadFailure' => 'دانلود ناموفق بود',
+    'uninstallFailure' => 'حذف نصب ناموفق بود',
+
+    'copySuccess' => 'کپی با موفقیت انجام شد',
+    'viewLog' => 'مشکلی در اجرا رخ داده است، لطفاً برای جزئیات به گزارش سیستم Fresns مراجعه کنید',
+    // auth empty
+    'auth_empty_title' => 'لطفاً از پورتال صحیح برای ورود به پنل استفاده کنید',
+    'auth_empty_description' => 'شما از سیستم خارج شده‌اید یا زمان ورود شما به پایان رسیده است، لطفاً به پورتال ورود مراجعه کنید.',
+    // request
+    'request_in_progress' => 'در حال پردازش درخواست...',
+    'requestSuccess' => 'درخواست با موفقیت انجام شد',
+    'requestFailure' => 'درخواست ناموفق بود',
+    // install
+    'install_not_entered_key' => 'لطفاً کلید Fresns را وارد کنید',
+    'install_not_entered_directory' => 'لطفاً یک پوشه وارد کنید',
+    'install_not_upload_zip' => 'لطفاً بسته نصب را انتخاب کنید',
+    'install_in_progress' => 'در حال نصب...',
+    'install_end' => 'پایان نصب',
+    // upgrade
+    'upgrade_none' => 'هیچ ارتقایی وجود ندارد',
+    'upgrade_fresns' => 'نسخه جدیدی از Fresns برای ارتقاء در دسترس است',
+    'upgrade_fresns_tip' => 'می‌توانید به نسخه جدید ارتقا دهید',
+    'upgrade_fresns_warning' => 'لطفاً قبل از ارتقاء از دیتابیس نسخه پشتیبان تهیه کنید تا از دست دادن داده‌ها جلوگیری شود.',
+    'upgrade_confirm_tip' => 'آیا مطمئن هستید که می‌خواهید ارتقا دهید؟',
+    'manual_upgrade_tip' => 'این بروزرسانی از ارتقاء خودکار پشتیبانی نمی‌کند، لطفاً از روش "ارتقاء دستی" استفاده کنید.',
+    'manual_upgrade_version_guide' => 'برای مشاهده توضیحات این نسخه کلیک کنید',
+    'manual_upgrade_guide' => 'راهنمای ارتقاء',
+    'manual_upgrade_file_error' => 'عدم تطابق فایل ارتقاء دستی',
+    'manual_upgrade_confirm_tip' => 'لطفاً مطمئن شوید که "راهنمای ارتقاء" را خوانده‌اید و فایل‌های نسخه جدید را طبق راهنما پردازش کرده‌اید.',
+    'upgrade_in_progress' => 'در حال ارتقاء...',
+    'auto_upgrade_step_1' => 'تأیید اولیه',
+    'auto_upgrade_step_2' => 'دانلود بسته‌های برنامه',
+    'auto_upgrade_step_3' => 'باز کردن بسته برنامه',
+    'auto_upgrade_step_4' => 'ارتقاء برنامه',
+    'auto_upgrade_step_5' => 'پاکسازی کش',
+    'auto_upgrade_step_6' => 'پایان',
+    'manualUpgrade_step_1' => 'تأیید اولیه',
+    'manualUpgrade_step_2' => 'به‌روزرسانی داده‌ها',
+    'manualUpgrade_step_3' => 'نصب تمامی بسته‌های وابستگی پلاگین',
+    'manualUpgrade_step_4' => 'فعال‌سازی و بازگردانی افزونه‌ها',
+    'manualUpgrade_step_5' => 'به‌روزرسانی اطلاعات نسخه Fresns',
+    'manualUpgrade_step_6' => 'پاکسازی کش',
+    'manualUpgrade_step_7' => 'پایان',
+    // uninstall
+    'uninstall_in_progress' => 'در حال حذف نصب...',
+    'uninstall_step_1' => 'تأیید اولیه',
+    'uninstall_step_2' => 'پردازش داده‌ها',
+    'uninstall_step_3' => 'حذف فایل‌ها',
+    'uninstall_step_4' => 'پاکسازی کش',
+    'uninstall_step_5' => 'پایان',
+    // select
+    'select_box_tip_plugin' => 'پلاگین را انتخاب کنید',
+    'select_box_tip_role' => 'نقش را انتخاب کنید',
+    'select_box_tip_group' => 'گروه را انتخاب کنید',
+    'post_datetime_select_error' => 'بازه زمانی تنظیم شده برای پست نمی‌تواند خالی باشد',
+    'post_datetime_select_range_error' => 'زمان پایان تنظیمات پست نمی‌تواند کمتر از زمان شروع باشد',
+    'comment_datetime_select_error' => 'بازه زمانی تنظیم شده برای نظر نمی‌تواند خالی باشد',
+    'comment_datetime_select_range_error' => 'زمان پایان تنظیمات نظر نمی‌تواند کمتر از زمان شروع باشد',
+    // delete app
+    'delete_app_warning' => 'اگر نمی‌خواهید هشدارهای بروزرسانی برنامه را ببینید، می‌توانید آن را حذف کنید. با این کار دیگر هشدارهای نسخه جدید دریافت نخواهید کرد.',
+    // dashboard
+    'panel_config' => 'پس از تغییر تنظیمات، باید کش پاک شود تا تنظیمات جدید اعمال شود.',
+    'plugin_install_or_upgrade' => 'پس از نصب یا ارتقاء پلاگین، وضعیت پیش‌فرض غیرفعال است و باید به صورت دستی فعال شود تا از مشکلات سیستم جلوگیری شود.',
+    // website
+    'website_path_empty_error' => 'ذخیره ناموفق بود، پارامتر مسیر نباید خالی باشد',
+    'website_path_format_error' => 'ذخیره ناموفق بود، پارامتر مسیر تنها باید شامل حروف انگلیسی باشد',
+    'website_path_reserved_error' => 'ذخیره ناموفق بود، پارامتر مسیر شامل نام‌های رزرو شده سیستمی است',
+    'website_path_unique_error' => 'ذخیره ناموفق بود، پارامترهای مسیر نباید تکراری باشند',
+    // theme
+    'website_engine_error' => 'موتور وب‌سایت نصب نشده است',
+    'theme_error' => 'قالب اشتباه است یا وجود ندارد',
+    'theme_functions_file_error' => 'فایل تنظیمات قالب اشتباه است یا وجود ندارد',
+    'theme_json_file_error' => 'فایل تنظیمات قالب اشتباه است یا وجود ندارد',
+    'theme_json_format_error' => 'فرمت فایل تنظیمات قالب اشتباه است',
+    // others
+    'markdown_editor' => 'محتوا از نگارش Markdown پشتیبانی می‌کند، اما باکس ورودی پیش‌نمایش ندارد. لطفاً ذخیره کنید تا تأثیر آن را مشاهده کنید.',
+    'account_not_found' => 'حساب کاربری وجود ندارد یا اشتباه وارد شده است',
+    'account_login_limit' => 'تعداد خطاها از حد مجاز سیستم فراتر رفته است. لطفاً یک ساعت بعد دوباره وارد شوید.',
+    'timezone_error' => 'منطقه زمانی دیتابیس با تنظیمات منطقه زمانی در فایل .env همخوانی ندارد',
+    'timezone_env_edit_tip' => 'لطفاً شناسه منطقه زمانی را در فایل .env ویرایش کنید',
+    'secure_entry_route_conflicts' => 'تعارض در مسیر ورود امن',
+    'language_exists' => 'زبان قبلاً وجود دارد',
+    'language_not_exists' => 'زبان وجود ندارد',
+    'plugin_not_exists' => 'پلاگین وجود ندارد',
+    'map_exists' => 'این ارائه‌دهنده خدمات نقشه قبلاً استفاده شده است و نمی‌توان دوباره ایجاد کرد',
+    'map_not_exists' => 'نقشه وجود ندارد',
+    'required_user_role_name' => 'لطفاً نام نقش را وارد کنید',
+    'required_sticker_category_name' => 'لطفاً نام دسته‌بندی استیکر را وارد کنید',
+    'required_group_name' => 'لطفاً نام گروه را وارد کنید',
+    'delete_default_language_error' => 'زبان پیش‌فرض قابل حذف نیست',
+    'account_connect_services_error' => 'پلتفرم‌های تکراری در پشتیبانی اتصال شخص ثالث وجود دارد',
+];
