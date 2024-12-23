e9ddafb1630a4f1b936f2461f98bf2d17e44f94b
Jevan <22316055+jevantang@users.noreply.github.com>
2024-12-23 09:58:39 +0800

    chore: framework config

--- config/database.php
+++ config/database.php
@@ -72,7 +72,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_520_ci'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
             'prefix' => env('DB_PREFIX', ''),
             'prefix_indexes' => true,
             'strict' => true,
